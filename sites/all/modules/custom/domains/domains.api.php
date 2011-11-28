6ff8e982b70601f8f4ac5d98f5422bf66575bad1
Andy T <andychristiantruong@gmail.com>
2011-11-29 02:20:05 +0700

    update doc for hook_domains_controller_info().

--- sites/all/modules/custom/domains/domains.api.php
+++ sites/all/modules/custom/domains/domains.api.php
@@ -6,6 +6,9 @@
 
 /**
  * Implements this hook to provide your controler for your domain provider.
+ * 
+ * Return array of domain providers, each element is keyed-array, with
+ *  $element['class'] is name of class implemented DomainControllerInterface.
  */
 function hook_domains_controller_info() {
   return array(
