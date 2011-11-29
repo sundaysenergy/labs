ca73f8dcde0e075c2a816a395730e792bf136ba3
Bao Nguyen <contact@nqbao.com>
2011-11-29 10:30:26 +0700

    Pull domains from Drupal repo. Rename domain entity to domains_domain to completely remove domain_* namespace.

--- sites/all/modules/custom/domains/domains.api.php
+++ sites/all/modules/custom/domains/domains.api.php
@@ -8,12 +8,14 @@
  * Implements this hook to provide your controler for your domain provider.
  * 
  * Return array of domain providers, each element is keyed-array, with
- *  $element['class'] is name of class implemented DomainControllerInterface.
+ *  $element['class'] is name of class implemented DomainProviderInterface.
  */
-function hook_domains_controller_info() {
+function hook_domains_provider_info() {
   return array(
     'enom' => array(
+      'label' => 'Enom',
       'class' => 'EnomDomainController',
+      'ui class' => 'EnomDomainUiController',
     ),
   );
 }
